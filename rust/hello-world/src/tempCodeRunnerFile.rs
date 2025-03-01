eae8e42b4dc5c1f6af2b6ad55c848e2cc5bd4c5b
Mohammed <upscsskzm805@gmail.com>
2025-03-01 16:33:03 +0530

    Initial commit

---
+++ rust/hello-world/src/tempCodeRunnerFile.rs
@@ -0,0 +1,11 @@
+// &'static is a "lifetime specifier", something you'll learn more about later
+pub fn hello() -> &'static str {
+    "Hello, World!"
+    println!
+    (
+        "Hello, World!"
+    )
+}
+fn main(){
+    hello();
+}
