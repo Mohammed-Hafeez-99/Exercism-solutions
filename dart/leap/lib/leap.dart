6119018f1010e4a6e25524e77f84599a35c14f7c
Mohammed-Hafeez-99 <upscsskzm805@gmail.com>
2025-03-27 18:19:35 +0530

    on progress

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,3 +1,9 @@
+import 'dart.io';
+
 class Leap {
-  // Put your code here
+stdout.write('enter a number');
+String? input = stdin.readLineSync();
+if(input%4==0&&input%400==0){
+  stdout.write("it is a leap year");
+}
 }
